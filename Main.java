cae7c277980c6d486c1e99f242d2e58fba12f8d1
Denis Kochetkov <denis_ka_2307@mail.ru>
2022-02-16 21:27:58 +0300

    add file from prev version of engine. Main, Object, Light, Material, Voxel. Set settings .gitignore: ignore .class and .bak files

---
+++ Main.java
@@ -0,0 +1,183 @@
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JFrame;
+import javax.swing.RepaintManager;
+
+
+import java.util.ArrayList;
+
+
+class Main extends Component{
+	private static int fps_time = 70;
+	
+	private BufferedImage buffer_screen;
+	private double k_width;
+	
+	private double fps = 0;
+	
+	Camera camera = new Camera(new Vector(200,0,0), new Vector(-4,0,0));
+	
+	private ArrayList<Object> mass = new ArrayList<Object>();
+	private PointLight l = new PointLight(new Vector(100, 100, 100));
+	
+	Material lite = new Material(255,255,255);
+	Material dark = new Material(128,128,128);
+	
+	
+	Main(int w, int h){
+		k_width = (double)w/h;
+		buffer_screen = new BufferedImage(w,h,BufferedImage.TYPE_INT_RGB);
+		this.setBounds(0,0,w,h);
+		MainListener ml = new MainListener();
+		this.addKeyListener(ml);
+		this.addMouseListener(ml);
+		this.addMouseMotionListener(ml);
+		
+		mass.add(new Cube(new Vector(0,20,0), 10));
+		mass.add(new Sphere(Vector.ZERO, 10));
+		mass.add(new Plane(new Vector(0,0,1), -30));;
+	}
+	
+	public void getFps(long delt){
+		fps = 1000.0/delt;
+	}
+	
+	private void updateScreenBuffer(){
+		for(int x = -this.getWidth()/2; x< this.getWidth()/2; x++){
+			for(int y = -this.getHeight()/2; y< this.getHeight()/2; y++){
+				getPixel(x,y);
+			}
+		}
+	}
+	private void getPixel(int x, int y){
+		int a = 0;
+		
+		Vector view = Vector.getOne(Vector.sum(camera.alpha, Vector.multiply(new Vector(0,x/(double)this.getWidth()*k_width,-y/(double)this.getHeight()), Vector.getLength(this.camera.alpha))));
+		
+		view = Vector.rotateVector(view, camera.a_z, camera.a_y);
+		double t = -1;
+		int index = 0;
+		for(int i = 0; i< this.mass.size(); i++){
+			double t1 = this.mass.get(i).getMinIntersection(this.camera.cord,view);
+			if(t1>0){
+				if(t1< t || t == -1){
+					t = t1;
+					index = i;
+				}
+			}
+		}
+		if(t>0 && t< 10000){
+			Vector dote = Vector.sum(this.camera.cord, Vector.multiply(view, t));
+			Vector n = this.mass.get(index).getNormal(dote);
+			double k = Vector.scalarMul(Vector.getOne(n), Vector.getOne(Vector.negate(this.l.cord, dote)));
+			if(k<0) k = 0;
+			a = Material.getUnityColor(k);
+			
+		}
+		this.buffer_screen.setRGB(x+this.getWidth()/2, y+this.getHeight()/2, a);
+	}
+	/*private double checkIntersection(Vector c, Vector v){				// check intersection of vector with all objects
+		double t = -1;
+		for(int i = 0; i< this.mass.size(); i++){
+			double t1 = this.mass.get(i).getMinIntersection(c,v);
+			if(t1>0){
+				if(t1< t || t == -1){
+					t = t1;
+				}
+			}
+		}
+		return t;
+	}*/
+	
+	@Override
+	public void paint(Graphics g){ 										// function of paint.
+		updateScreenBuffer();
+		g.drawImage(buffer_screen, 0,0,null);
+		g.setColor(Color.white);
+		g.drawString("fps: "+ fps, 0,15);
+	}
+	
+	
+	public static void main(String[] args){								// initialize
+		JFrame f = new JFrame();
+		f.setSize(1280,620);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Main g = new Main(f.getWidth(), f.getHeight());
+		g.setFocusable(true);
+		f.add(g);
+		
+		f.setLayout(null);
+		f.setVisible(true);
+		
+		RepaintManager rm = new RepaintManager();
+		long delt = 0;
+		while(true){//mainloop
+			long d0 = System.currentTimeMillis();
+			f.repaint(0,0,f.getWidth(),f.getHeight());
+			g.getFps(delt);
+			rm.paintDirtyRegions();
+			delt = System.currentTimeMillis() - d0;
+			try{
+				Thread.currentThread().sleep(fps_time - delt);
+				delt = fps_time;
+			}catch(Exception e){}
+		}
+	}
+	
+	class MainListener implements KeyListener, MouseMotionListener, MouseListener{							// system of control
+		private double x0 = 0;
+		private double y0 = 0;
+		
+		
+		public void keyPressed(KeyEvent e){
+			if(e.getKeyCode() == KeyEvent.VK_W){
+				camera.cord.x -= 5;
+			}else if(e.getKeyCode() == KeyEvent.VK_S){
+				camera.cord.x += 5;
+			}
+			if(e.getKeyCode() == KeyEvent.VK_A){
+				camera.cord.y -= 5;
+			}else if(e.getKeyCode() == KeyEvent.VK_D){
+				camera.cord.y += 5;
+			}
+			if(e.getKeyCode() == KeyEvent.VK_SPACE){
+				camera.cord.z += 5;
+			}else if(e.getKeyCode() == KeyEvent.VK_SHIFT){
+				camera.cord.z -= 5;
+			}
+			if(e.getKeyCode() == KeyEvent.VK_1){
+				camera.a_y += 0.1;
+			}else if(e.getKeyCode() == KeyEvent.VK_2){
+				camera.a_y -= 0.1;
+			}
+		}
+		public void keyTyped(KeyEvent e){}
+		public void keyReleased(KeyEvent e){}
+		
+		public void mouseExited(MouseEvent e){}
+		public void mouseEntered(MouseEvent e){}
+		public void mouseClicked(MouseEvent e){}
+		public void mousePressed(MouseEvent e){this.x0 = e.getX(); this.y0 = e.getY();}
+		public void mouseReleased(MouseEvent e){}
+		
+		public void mouseDragged(MouseEvent e){
+			//System.out.println(this.x0 + " "+this.y0);
+			camera.a_y += (e.getY()-y0)/200.0;
+			camera.a_z -= (e.getX() - x0)/200.0;
+			this.x0 = e.getX(); this.y0 = e.getY();
+		}
+		public void mouseMoved(MouseEvent e){}
+	}
+}
+
---
+++ Object.java
@@ -0,0 +1,245 @@
+
+
+class Vector{
+	public static final Vector ZERO = new Vector(0,0,0);
+	public static final Vector i = new Vector(1,0,0);
+	public static final Vector j = new Vector(0,1,0);
+	public static final Vector k = new Vector(0,0,1);
+	
+	public double x;
+	public double y;
+	public double z;
+	
+	Vector(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public static Vector sum(Vector v1, Vector v2){
+		return new Vector(v1.x+v2.x, v1.y+v2.y, v1.z+v2.z);
+	}
+	public static Vector negate(Vector v1, Vector v2){
+		return new Vector(v1.x-v2.x, v1.y-v2.y, v1.z-v2.z);
+	}
+	public static Vector multiply(Vector v, double k){
+		return new Vector(v.x*k, v.y*k, v.z*k);
+	}
+	public static double scalarMul(Vector v1, Vector v2){
+		return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+	}
+	public static Vector vectorMul(Vector v1, Vector v2){
+		return new Vector(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y-v2.x);
+	}
+	public static double getLength(Vector v){
+		return Math.sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+	}
+	public static Vector getOne(Vector v){
+		return Vector.multiply(v, 1/Vector.getLength(v));
+	}
+	public static Vector rotateVector(Vector v, double a_z, double a_y){
+		Vector a = new Vector(v.x, v.y, v.z);
+		
+		/*double l = Vector.getLength(a);
+		double k = Math.cos(a_y) - v.z* Math.sin(a_y)/l;
+		a = Vector.multiply(a, k);
+		a.z = v.z*Math.cos(a_y) + l* Math.sin(a_y);
+		
+		double x = a.x;
+		double y = a.y;
+		a.x = x*Math.cos(a_z) + y * Math.sin(a_z);
+		a.y = y*Math.cos(a_z) - x * Math.sin(a_z);*/
+		
+		double cos = a.x;
+		double sin = a.z;
+		a.x = cos*Math.cos(a_y) - sin*Math.sin(a_y);
+		a.z = cos*Math.sin(a_y) + sin*Math.cos(a_y);
+		
+		cos = a.x;
+		sin = a.y;
+		a.x = cos*Math.cos(a_z) - sin*Math.sin(a_z);
+		a.y = cos*Math.sin(a_z) + sin*Math.cos(a_z);
+		
+		return a;
+	}
+}
+
+abstract class Object{
+	public Vector cord;
+	
+	abstract public double getMinIntersection(Vector c, Vector v);
+	abstract public double getMaxIntersection(Vector c, Vector v);
+	abstract public Vector getNormal(Vector dote);
+}
+
+
+class Camera{
+	public Vector cord;
+	public Vector alpha;
+	
+	public double a_z = 0;
+	public double a_y = 0;
+	
+	Camera(Vector c, Vector n){
+		this.cord = c;
+		this.alpha = n;
+	}
+}
+
+
+class Sphere extends Object{
+	public double r;
+	
+	Sphere(Vector c, double x){
+		this.cord = c;
+		this.r = x;
+	}
+	public double getMinIntersection(Vector c, Vector v){
+		Vector d = Vector.negate(c, this.cord);
+		double a1 = v.x*v.x + v.y*v.y + v.z*v.z;
+		double b1 = Vector.scalarMul(v, d);
+		double c1 = d.x*d.x + d.y*d.y + d.z*d.z - this.r*this.r;
+		
+		double D = b1*b1 - a1*c1;
+		if(D<0) return -1;
+		D = Math.sqrt(D);
+		double t = (-1)*(D+b1)/a1;
+		if(t>=-0.001) return t;
+		t = (D-b1)/a1;
+		if(t>=-0.001) return t;
+		return -1;
+	}
+	public double getMaxIntersection(Vector c, Vector v){
+		Vector d = Vector.negate(c, this.cord);
+		double a1 = v.x*v.x + v.y*v.y + v.z*v.z;
+		double b1 = Vector.scalarMul(v, d);
+		double c1 = d.x*d.x + d.y*d.y + d.z*d.z - this.r*this.r;
+		
+		double D = b1*b1 - a1*c1;
+		if(D<0) return -1;
+		D = Math.sqrt(D);
+		double t = (D-b1)/a1;
+		if(t>=-0.001) return t;
+		t = -(D+b1)/a1;
+		if(t>=-0.001) return t;
+		return -1;
+	}
+	public Vector getNormal(Vector dote){
+		return Vector.negate(dote, this.cord);
+	}
+}
+class Cube extends Object{
+	public double a;
+	
+	Cube(Vector c, double x){
+		this.cord = c;
+		this.a = x;
+	}
+	
+	public double getMinIntersection(Vector c, Vector v){
+		Vector d = Vector.negate(this.cord, c);
+		
+		double tx1 = (d.x + this.a)/v.x;
+		double tx2 = (d.x - this.a)/v.x;
+		if(tx1>tx2){
+			double t = tx1;
+			tx1 = tx2;
+			tx2 = t;
+		}
+		double tx = tx1;
+		if(tx<0) tx = tx2;
+		
+		double ty1 = (d.y + this.a)/v.y;
+		double ty2 = (d.y - this.a)/v.y;
+		if(ty1>ty2){
+			double t = ty1;
+			ty1 = ty2;
+			ty2 = t;
+		}
+		double ty = ty1;
+		if(ty<0) ty = ty2;
+		
+		double tz1 = (d.z + this.a)/v.z;
+		double tz2 = (d.z - this.a)/v.z;
+		if(tz1>tz2){
+			double t = tz1;
+			tz1 = tz2;
+			tz2 = t;
+		}
+		double tz = tz1;
+		if(tx<0) tz = tz2;
+		
+		double t = -1;
+		if(tx>= ty1 && tx<= ty2 && tx>= tz1 && tx<= tz2 && (tx<t || t == -1)) t = tx;
+		if(ty>= tx1 && ty<= tx2 && ty>= tz1 && ty<= tz2 && (ty<t || t == -1)) t = ty;
+		if(tz>= ty1 && tz<= ty2 && tz>= tx1 && tz<= tx2 && (tz<t || t == -1)) t = tz;
+		return t;
+	}
+	public double getMaxIntersection(Vector c, Vector v){
+		Vector d = Vector.negate(c, this.cord);
+		
+		double tx1 = (d.x + this.a)/v.x;
+		double tx2 = (d.x - this.a)/v.x;
+		if(tx1>tx2){
+			double t = tx1;
+			tx1 = tx2;
+			tx2 = t;
+		}
+		double tx = tx2;
+		if(tx<0) tx = tx1;
+		
+		double ty1 = (d.y + this.a)/v.y;
+		double ty2 = (d.y - this.a)/v.y;
+		if(ty1>ty2){
+			double t = ty1;
+			ty1 = ty2;
+			ty2 = t;
+		}
+		double ty = ty2;
+		if(ty<0) ty = ty1;
+		
+		double tz1 = (d.z + this.a)/v.z;
+		double tz2 = (d.z - this.a)/v.z;
+		if(tz1>tz2){
+			double t = tz1;
+			tz1 = tz2;
+			tz2 = t;
+		}
+		double tz = tz2;
+		if(tx<0) tz = tz1;
+		
+		double t = -1;
+		if(tx>= ty1 && tx<= ty2 && tx>= tz1 && tx<= tz2 && tx>t) t = tx;
+		if(ty>= tx1 && ty<= tx2 && ty>= tz1 && ty<= tz2 && ty>t) t = ty;
+		if(tz>= ty1 && tz<= ty2 && tz>= tx1 && tz<= tx2 && tz>t) t = tz;
+		return t;
+	}
+	public Vector getNormal(Vector dote){
+		if(Math.abs(dote.x - this.cord.x-this.a) < 0.01) return Vector.i;
+		if(Math.abs(dote.x - this.cord.x+this.a) < 0.01) return new Vector(-1, 0,0);
+		if(Math.abs(dote.y - this.cord.y-this.a)<0.01) return Vector.j;
+		if(Math.abs(dote.y - this.cord.y+this.a)<0.01) return new Vector(0, -1,0);
+		if(Math.abs(dote.z - this.cord.z-this.a)<0.01) return Vector.k;
+		if(Math.abs(dote.z - this.cord.z+this.a)<0.01) return new Vector(0, 0,-1);
+		return Vector.i;
+	}
+}
+
+class Plane extends Object{
+	public double d;
+	
+	Plane(Vector x, double y){
+		this.cord = x; // normal vector
+		this.d = -y;
+	}
+	public double getMinIntersection(Vector c, Vector v){
+		double t = -(Vector.scalarMul(this.cord, c ) + d)/Vector.scalarMul(this.cord, v);
+		return t;
+	}
+	public double getMaxIntersection(Vector c, Vector v){
+		return getMinIntersection(c,v);
+	}
+	public Vector getNormal(Vector dote){
+		return this.cord;
+	}
+}
